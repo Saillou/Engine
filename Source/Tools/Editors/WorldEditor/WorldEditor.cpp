fc9341d3793aec0439abd38bffd1febe1e33b226
Saillou <chauvinremy@upyourprod.fr>
2024-02-07 14:26:50 +0100

    utilities functions

--- Source/Tools/Editors/WorldEditor/WorldEditor.cpp
+++ Source/Tools/Editors/WorldEditor/WorldEditor.cpp
@@ -1,7 +1,35 @@
 #include "WorldEditor.h"
 
-#include <utility>
+// -------- Utilities --------
+static Entity __create_tile_with_texture(const std::string& texturePath) {
+    Entity entity(Entity::SimpleShape::Quad);
 
+    // Transform:
+    //  - translate on z for having the ground level at 0
+    //  - rotate for having Y as depth (instead of Z because i prefer)
+    entity.localPose() = glm::rotate(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 1.0f)), glm::pi<float>() / 2.0f, glm::vec3(1, 0, 0));
+
+    // Define opacity < 1: trick renderer with reordering
+    entity.localMaterial() = Material{ glm::vec4(0, 0, 0, 0.9f), false }; 
+
+    // Set texture on the first (and only) mesh
+    entity.model().root()->meshes.front()->textures().push_back(
+        TextureData{ "texture_diffuse", std::make_unique<Texture>(texturePath) }
+    );
+
+    return entity;
+}
+
+static Entity __create_tile_with_rgba(const glm::vec4& colorRGBA) {
+    Entity entity(Entity::SimpleShape::Quad);
+
+    // channel color 8-bits to float
+    entity.localMaterial() = Material{ colorRGBA / 255.0f, false };
+
+    return entity;
+}
+
+// -------- Editor --------
 WorldEditor::WorldEditor(Scene& scene) : 
     Editor(scene)
 { }
@@ -20,16 +48,18 @@ void WorldEditor::onEnter()
     m_scene.camera().position  = glm::vec3(0.0f, -5.0f, 0.15f);
     m_scene.camera().direction = glm::vec3(0, 0, 0);
 
-    // Create quad with texture
-    m_entities["quad"] = Entity(Entity::SimpleShape::Quad);
-    m_entities["quad"].model().root()->meshes.front()->textures().push_back(
-        TextureData{"texture_diffuse", std::make_unique<Texture>("Resources/textures/grass.png")}
-    );
-    m_entities["quad"].localPose()     = glm::rotate(glm::mat4(1.0f), glm::pi<float>() / 2.0f, glm::vec3(1, 0, 0));
-    m_entities["quad"].localMaterial() = Material{ glm::vec4(1, 0, 0, 0.9f), false };
-    m_entities["quad"].poses() = {
-        glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.5f)), glm::vec3(-0.25f, 0.1f, 0.f)),
-        glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.5f)), glm::vec3(+0.25f, 0.2f, 0.f)),
+    // Create tiles
+    m_entities["grass"] = __create_tile_with_texture("Resources/textures/grass.png");
+    m_entities["earth"] = __create_tile_with_rgba(glm::vec4(185, 122, 87, 255));
+
+    // Create world
+    m_entities["grass"].poses() = {
+        glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(0.5f)), glm::vec3(-0.25f, 0.1f, 0.0f)),
+        glm::translate(glm::scale(glm::mat4(1.0f), glm::vec3(1.0f)), glm::vec3(+0.25f, 0.2f, 0.0f)),
+    };
+
+    m_entities["earth"].poses() = {
+        glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, 0.0f)),
     };
 }
 
